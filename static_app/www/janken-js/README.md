fcc93593cb94abdaf28555371251c5e71d5bce3c
osakana0000 <119655769+osakana0000@users.noreply.github.com>
2023-02-14 14:18:30 +0000

    add janken-js files

---
+++ static_app/www/janken-js/README.md
@@ -0,0 +1,26 @@
+# janken-js
+## Overview
+- じゃんけんを模したWebアプリです。
+
+## Demo
+URL [https://osakana0000.github.io/jankenJs/](http://osakana0000.site/janken-js/)
+
+## Discription
+- 「グー」「チョキ」「パー」のいづれかを選択すると、CPUもじゃんけんの手段を表示します。
+- 「ランダム」は、プレイヤーの手段をランダムで表示します。
+
+## Features
+- 勝敗の判定は以下の通りです。
+  - プレイヤーとCPUの手段が同じなら「あいこ」
+  - 勝ちの組み合わせ3パターンいずれかに該当すれば「勝ち」
+  - 上記以外は「負け」
+
+## Requirement
+- ES6
+- Bootstrap5
+
+## Licence
+- This code in under [MIT license](https://en.wikipedia.org/wiki/MIT_License)
+
+## Author
+- osakana0000 [https://github.com/osakana0000](https://github.com/osakana0000)
--- static_app/www/janken-js/script.js
+++ static_app/www/janken-js/script.js
@@ -1,59 +1,100 @@
+// 全戦闘数
+let counter = 0;
+// 勝ちの数
+let winCnt = 0;
+
+// ジェスチャー
+const rock = document.getElementById("rock");
+const scissors = document.getElementById("scissors");
+const paper = document.getElementById("paper");
+const cpu = document.getElementById("cpu");
+const reset = document.getElementById("reset");
+
+// 勝敗数
+const count = document.getElementById("count");
+// ジェスチャーの表示
+const fig = document.getElementById("fig");
+// 勝敗のメッセージ
+const message = document.getElementById("message");
+
+// 数字とジェスチャーを紐図ける
+const handFig = { 1: "グー", 2: "チョキ", 3: "パー" };
+// 勝敗の判定
+// 負け: 0
+// あいこ: 1
+// かち： 2
+
 const matchPattern = (player, cpu) => {
+  // 勝ちになる場合のリスト
   const patterns = [
     [1, 2],
     [2, 3],
     [3, 1],
   ];
+
+  // あいこの判定
   if (player === cpu) {
     return 1;
   }
 
+  // 勝ちの判定
   for (const pattern of patterns) {
+    // patternsに合致する組み合わせがあれば勝ちの判定を下す
     if (player === pattern[0] && cpu === pattern[1]) {
       return 2;
     }
   }
+
+  // どの判定にも通らなければ負けの判定
   return 0;
 };
 
-window.addEventListener("DOMContentLoaded", () => {
-  let counter = 0;
-  let winCnt = 0;
-  const rock = document.getElementById("rock");
-  const scissors = document.getElementById("scissors");
-  const paper = document.getElementById("paper");
-  const cpu = document.getElementById("cpu");
-  // const reset = document.getElementById("reset");
+// player: 入力
+// who:
+const main = (player, who) => {
+  // CPUのジェスチャーをランダムに生成(1~3)
+  const cpu = Math.floor(Math.random() * 3) + 1;
 
-  const main = (player, who) => {
-    const counterMsg = document.getElementById("counterMsg");
-    const message = document.getElementById("message");
-    const handFig = {1: "グー", 2: "チョキ", 3: "パー"};
-    const cpu = 1 + Math.floor(Math.random() * 3);
-    const judge = matchPattern(player, cpu);
-    counterMsg.innerHTML = `${counter}戦目！　${winCnt}勝${counter - winCnt}敗中！`;
+  // プレーヤーとCPUの勝敗を判定する
+  // 負け: 0
+  // あいこ: 1
+  // かち： 2
+  const judge = matchPattern(player, cpu);
 
-    let judgeMsg = `<p>${who}：${handFig[player]}　CPU：${handFig[cpu]}</p>`;
+  // プレイヤーとCPUのジェスチャーを表示
+  fig.innerText = `${who}：${handFig[player]}　CPU：${handFig[cpu]}`;
 
-    if (judge === 2) {
-      winCnt += 1;
-      counter += 1;
-      judgeMsg += "<p>あなたの勝ち！</p>";
-    } else if (judge === 1) {
-      judgeMsg += "<p>あいこ</p>";
-    } else {
-      judgeMsg += "<p>あなたの負け．．．</p>";
-      counter += 1;
-    }
-    message.innerHTML = judgeMsg;
-  };
+  // 勝敗に応じて更新
+  if (judge === 2) {
+    winCnt += 1;
+    counter += 1;
+    message.innerText = "あなたの勝ち！";
+  } else if (judge === 1) {
+    message.innerText = "あいこ";
+  } else {
+    message.innerText = "あなたの負け．．．";
+    counter += 1;
+  }
 
-  rock.addEventListener("click", () => main(1, "あなた"));
-  scissors.addEventListener("click", () => main(2, "あなた"));
-  paper.addEventListener("click", () => main(3, "あなた"));
+  // 勝敗数の表示を更新
+  count.innerHTML = `${counter}戦目！　${winCnt}勝${counter - winCnt}敗中！`;
+};
 
-  cpu.addEventListener("click", () => {
-    const cpu = 1 + Math.floor(Math.random() * 3);
-    main(cpu, "あなた");
-  });
+// 各ボタンのイベントリスナー
+rock.addEventListener("click", () => main(1, "あなた"));
+scissors.addEventListener("click", () => main(2, "あなた"));
+paper.addEventListener("click", () => main(3, "あなた"));
+cpu.addEventListener("click", () => {
+  // プレイヤーの代わりとなってジェスチャーをランダムに決める
+  const cpuFig = 1 + Math.floor(Math.random() * 3);
+  main(cpuFig, "あなた（ランダム）");
+});
+
+// 「初期化する」ボタンが押されたとき
+reset.addEventListener("click", () => {
+  counter = 0;
+  winCnt = 0;
+  count.innerText = "0戦目！　0勝0敗中！";
+  fig.innerText = "ボタンを選んでスタート！";
+  message.innerText = "じゃん．．．けん．．．";
 });
